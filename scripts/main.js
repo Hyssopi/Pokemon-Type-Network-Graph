e3a2c55d96ce461674031400af49322d7bcaa3d2
Hyssopi <43843070+Hyssopi@users.noreply.github.com>
2022-03-19 20:55:40 -0700

    Change to use other average color function

--- scripts/main.js
+++ scripts/main.js
@@ -137,7 +137,7 @@ function translateNodeData(typeData, pokemonData)
       "name": pokemonData[i].name,
       "label": pokemonData[i].description,
       "image": image,
-      "color": utilities.calculateAverageColor(typeColors),
+      "color": utilities.calculateAverageColorRGBHex(typeColors),
       "group": "POKEMON"
     });
   }
